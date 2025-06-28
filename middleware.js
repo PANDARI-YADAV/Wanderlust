54e020a1c4345a7573130f3c8d28dbfa951dd31f
PANDARI-YADAV <pandaripaladi@gmail.com>
2025-06-29 03:38:05 +0530

    Updated UI styles and made show page responsive

--- middleware.js
+++ middleware.js
@@ -7,7 +7,7 @@ const ExpressError = require("./utils/ExpressError.js");
 module.exports.isLoggedIn = (req,res,next)=>{
   if(!req.isAuthenticated()){
     req.session.redirectUrl=req.originalUrl;
-    req.flash("error","You must be Logged in to create list");
+    req.flash("error","You must be Logged in");
    return res.redirect("/login");
   }
   next();
